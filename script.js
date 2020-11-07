a6335d5ab154199a08bcb34f8ab517256c2de752
Russell Jewett <rfjewett3@gmail.com>
2020-11-07 15:57:47 -0500

    Adds more logic to the generate function. Still needs work.

--- script.js
+++ script.js
@@ -4,65 +4,68 @@ var generateBtn = document.querySelector("#generate");
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-// Create the prompt for the user to set the length of the password.
-passLength = prompt("How long would you like your password to be? Please choose a number between 8 and 128.");
-if (prompt < 8 || prompt > 128) {
-  alert("Invalid number. Please enter a number between 8 and 128.");
-}
+// Write password to the #password input
+function writePassword() {
+  // var passwordGen = 
+  generatePassword();
+  var passwordText = document.querySelector("#password");
 
-// Create character possibilities using ACCII system.
-var capStart = 65;
-var lowStart = 97;
-var numStart = 48;
-var special = "!#$%&')(*+,-./:;<=>?@]/[^_`}|{~";
-
-// Creating the alerts for the user to tailor the password characters.
-cap = alert("Do you want the password to contain capital letters?");
-low = alert("Do you want the password to contain lower-case letters?");
-num = alert("Do you want the password to contain numbers?");
-spec = alert("Do you want the password to contain special characters?");
-
-// If none of the character alerts are answered, send an error message.
-if (cap != true && low != true && num != true && spec != true) {
-  alert("Error, no character selection. Please try again.")
-}
-
-// Create a function that gets a random number within a range.
-function getRandomInt(max) {
-  return Math.floor(Math.random() * Math.floor(max));
+  passwordText.value = password;
 }
 
 // Write the fuction that actually generates the password.
-function generatePassword() {
+function generatePassword(password) {
+
+  // Create the prompt for the user to set the length of the password.
+  passLength = prompt("How long would you like your password to be? Please choose a number between 8 and 128.");
+  if (passLength < 8 || passLength > 128) {
+    alert("Invalid number. Please enter a number between 8 and 128.");
+    generatePassword();
+  }
+
+  // Creating the alerts for the user to tailor the password characters.
+  var cap = prompt("Do you want the password to contain capital letters? y / n");
+  var low = prompt("Do you want the password to contain lower-case letters? y / n");
+  var num = prompt("Do you want the password to contain numbers? y / n");
+  var spec = prompt("Do you want the password to contain special characters? y / n");
+
+  // If none of the character alerts are answered, send an error message.
+  if (cap == "n" && low == "n" && num == "n" && spec == "n") {
+    alert("Error, no character selection. Please try again.")
+    generatePassword();
+  }
+
+  // Create character possibilities using ACCII system.
+  var capStart = 65;
+  var lowStart = 97;
+  var numStart = 48;
+  var special = "!#$%&')(*+,-./:;<=>?@]/[^_`}|{~";
+
+  // Create a function that gets a random number within a range.
+  function getRandomInt(max) {
+    return Math.floor(Math.random() * Math.floor(max));
+  }
+
   for (var i = 0; i < (passLength -1); i++) {
     while (true) {
       randNum = getRandomInt(4);
-      if (randNum == 0 && cap) {
+      if (randNum == 0 && cap == "y") {
         randNum = getRandomInt(26);
-        password = password.concat(string.fromCharCode(capStart + randNum));
+        password = password.concat(String.fromCharCode(capStart + randNum));
       }
-      else if (randNum == 1 && low) {
+      else if (randNum == 1 && low == "y") {
         randNum = getRandomInt(26);
-        password = password.concat(string.fromCharCode(lowStart + randNum));
+        password = password.concat(String.fromCharCode(lowStart + randNum));
       }
-      else if (randNum == 2 && num) {
+      else if (randNum == 2 && num == "y") {
         randNum = getRandomInt(10);
-        password = password.concat(string.fromCharCode(numStart + randNum));
+        password = password.concat(String.fromCharCode(numStart + randNum));
       }
-      else if (randNum == 3 && spec) {
+      else if (randNum == 3 && spec == "y") {
         randNum = getRandomInt(special.length);
         password = password.concat(special[randNum]);
       }
     }
   }
+  return password;
 };
-
-return password;
-
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-}
