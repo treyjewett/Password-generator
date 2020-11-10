9d087c5487e0b983e2f7a0387136c1b7ace7c09d
Russell Jewett <rfjewett3@gmail.com>
2020-11-09 20:01:16 -0500

    Adds some logic steps to aid the user if they choose to cancel.

--- script.js
+++ script.js
@@ -15,58 +15,81 @@ function writePassword() {
 // Write the fuction that actually generates the password.
 function generatePassword(password) {
 
+  var passLength = 0;
+  var cap = "";
+  var low = "";
+  var num = "";
+  var spec = "";
+
   // Create the prompt for the user to set the length of the password.
-  passLength = prompt("How long would you like your password to be? Please choose a number between 8 and 128.");
-  if (passLength < 8 || passLength > 128) {
-    alert("Invalid number. Please enter a number between 8 and 128. Click Generate Password to try again.");
-    return;
-  }
+  while (passLength < 8 || passLength > 128) {
+    passLength = prompt("How long would you like your password to be? Please choose a number between 8 and 128.");
+    if (passLength == null) {
+      break;
+    } else if (passLength < 8 || passLength > 128) {
+      alert("Invalid number. Please enter a number between 8 and 128. Click Generate Password to try again.");
+      return;
+    } else {
+      cap = prompt("Do you want the password to contain capital letters? y / n");
+      if (cap == null) {
+        break;
+      } else {
+        low = prompt(" Do you want the password to contain lower-case letters? y / n");
+        if (low == null) {
+          break;
+        } else {
+          num = prompt("Do you want the password to contain numbers? y / n");
+          if (num == null) {
+            break;
+          } else {
+            spec = prompt("Do you want the password to contain special characters? y / n");
+            if (spec == null) {
+              break;
+            } else {
+              if (cap == "n" && low == "n" && num == "n" && spec == "n") {
+                alert("Error, no character selection.")
+                return;
+              }
 
-  // Creating the alerts for the user to tailor the password characters.
-  var cap = prompt("Do you want the password to contain capital letters? y / n");
-  var low = prompt("Do you want the password to contain lower-case letters? y / n");
-  var num = prompt("Do you want the password to contain numbers? y / n");
-  var spec = prompt("Do you want the password to contain special characters? y / n");
+              // Create character possibilities using ACCII Standards system.
+              var capStart = 65;
+              var lowStart = 97;
+              var numStart = 48;
+              var special = "!#$%&')(*+,-./:;<=>?@]/[^_`}|{~";
 
-  // If none of the character alerts are answered, send an error message.
-  if (cap == "n" && low == "n" && num == "n" && spec == "n") {
-    alert("Error, no character selection. Click Generate Password to try again.")
-    return;
-  }
+              // Create a function that gets a random number within a range.
+              function getRandomInt(max) {
+                return Math.floor(Math.random() * Math.floor(max));
+              }
 
-  // Create character possibilities using ACCII system.
-  var capStart = 65;
-  var lowStart = 97;
-  var numStart = 48;
-  var special = "!#$%&')(*+,-./:;<=>?@]/[^_`}|{~";
-
-  // Create a function that gets a random number within a range.
-  function getRandomInt(max) {
-    return Math.floor(Math.random() * Math.floor(max));
-  }
-
-  for (var i = 0; i < passLength;) {
-    randNum = getRandomInt(4);
-    if (randNum == 0 && cap == "y") {
-      randNum = getRandomInt(26);
-      password = password.concat(String.fromCharCode(capStart + randNum));
-      i++;
+              for (var i = 0; i < passLength;) {
+                randNum = getRandomInt(4);
+                if (randNum == 0 && cap == "y") {
+                  randNum = getRandomInt(26);
+                  password = password.concat(String.fromCharCode(capStart + randNum));
+                  i++;
+                }
+                else if (randNum == 1 && low == "y") {
+                  randNum = getRandomInt(26);
+                  password = password.concat(String.fromCharCode(lowStart + randNum));
+                  i++
+                }
+                else if (randNum == 2 && num == "y") {
+                  randNum = getRandomInt(10);
+                  password = password.concat(String.fromCharCode(numStart + randNum));
+                  i++
+                }
+                else if (randNum == 3 && spec == "y") {
+                  randNum = getRandomInt(special.length);
+                  password = password.concat(special[randNum]);
+                  i++
+                }
+              }
+              return password;
+            }
+          }
+        }
+      }
     }
-    else if (randNum == 1 && low == "y") {
-      randNum = getRandomInt(26);
-      password = password.concat(String.fromCharCode(lowStart + randNum));
-      i++
-    }
-    else if (randNum == 2 && num == "y") {
-      randNum = getRandomInt(10);
-      password = password.concat(String.fromCharCode(numStart + randNum));
-      i++
-    }
-    else if (randNum == 3 && spec == "y") {
-      randNum = getRandomInt(special.length);
-      password = password.concat(special[randNum]);
-      i++
-    }
   }
-  return password;
 };
